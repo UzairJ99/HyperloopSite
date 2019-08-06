9f205f664e220906db03c4875a799dca9bd95d8c
Uzair Jawaid <uzair-jawaid@hotmail.com>
2019-08-05 23:34:15 -0400

    added JS file for logo minimzation on scroll

---
+++ scripts/minimizeLogo.js
@@ -0,0 +1,15 @@
+//Script to minimize logo in navbar on scroll
+//2019-08-05
+
+$(window).on('scroll', function () 
+		{
+			 var scrollTop = $(window).scrollTop();
+			 if (scrollTop > 40) 
+			 {
+					$('#logo').stop().animate({height: "65px", paddingLeft: "15px"},50);
+			 }
+			 else 
+			 {
+					$('#logo').stop().animate({height: "130px", paddingLeft: "0px"},50);  
+			 }
+		});
