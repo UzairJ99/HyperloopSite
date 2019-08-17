2e0c71c6b4a2a631ae252ec6f2ac7c0d2b99ba6d
Uzair Jawaid <uzair-jawaid@hotmail.com>
2019-08-17 00:25:33 -0400

    Added admin panel to team page and started adding sub team content

--- scripts/teamPage.js
+++ scripts/teamPage.js
@@ -4,9 +4,11 @@
 var softwareBtn = document.getElementById("softwareBtn");
 var electricalBtn = document.getElementById("electricalBtn");
 var mechanicalBtn = document.getElementById("mechanicalBtn");
+var adminBtn = document.getElementById("adminBtn");
 var softwarePanel = document.getElementById("softwarePanel");
 var mechanicalPanel = document.getElementById("mechanicalPanel");
 var electricalPanel = document.getElementById("electricalPanel");
+var adminPanel = document.getElementById("adminPanel");
 
 //add event listeners to the team buttons
 softwareBtn.addEventListener("click", function()
@@ -15,6 +17,7 @@ softwareBtn.addEventListener("click", function()
   softwareBtn.classList.add('active');
   mechanicalBtn.classList.remove('active');
   electricalBtn.classList.remove('active');
+  adminBtn.classList.remove('active');
   //make this panel visible
   softwarePanel.classList.add('visiblePanel');
   softwarePanel.classList.remove('hiddenPanel');
@@ -23,6 +26,8 @@ softwareBtn.addEventListener("click", function()
   mechanicalPanel.classList.add('hiddenPanel');
   electricalPanel.classList.remove('visiblePanel');
   electricalPanel.classList.add('hiddenPanel');
+  adminPanel.classList.remove('visiblePanel');
+  adminPanel.classList.add('hiddenPanel');
 });
 
 electricalBtn.addEventListener("click", function()
@@ -31,6 +36,7 @@ electricalBtn.addEventListener("click", function()
   softwareBtn.classList.remove('active');
   mechanicalBtn.classList.remove('active');
   electricalBtn.classList.add('active');
+  adminBtn.classList.remove('active');
   //make this panel visible
   electricalPanel.classList.add('visiblePanel');
   electricalPanel.classList.remove('hiddenPanel');
@@ -39,6 +45,8 @@ electricalBtn.addEventListener("click", function()
   mechanicalPanel.classList.add('hiddenPanel');
   softwarePanel.classList.remove('visiblePanel');
   softwarePanel.classList.add('hiddenPanel');
+  adminPanel.classList.remove('visiblePanel');
+  adminPanel.classList.add('hiddenPanel');
 });
 
 mechanicalBtn.addEventListener("click", function()
@@ -47,6 +55,7 @@ mechanicalBtn.addEventListener("click", function()
   softwareBtn.classList.remove('active');
   mechanicalBtn.classList.add('active');
   electricalBtn.classList.remove('active');
+  adminBtn.classList.remove('active');
   //make this panel visible
   mechanicalPanel.classList.add('visiblePanel');
   mechanicalPanel.classList.remove('hiddenPanel');
@@ -55,4 +64,25 @@ mechanicalBtn.addEventListener("click", function()
   electricalPanel.classList.add('hiddenPanel');
   softwarePanel.classList.remove('visiblePanel');
   softwarePanel.classList.add('hiddenPanel');
+  adminPanel.classList.remove('visiblePanel');
+  adminPanel.classList.add('hiddenPanel');
+});
+
+adminBtn.addEventListener("click", function()
+{
+  //highlight correct button
+  softwareBtn.classList.remove('active');
+  mechanicalBtn.classList.remove('active');
+  electricalBtn.classList.remove('active');
+  adminBtn.classList.add('active');
+  //make this panel visible
+  adminPanel.classList.add('visiblePanel');
+  adminPanel.classList.remove('hiddenPanel');
+  //hide the other panels
+  electricalPanel.classList.remove('visiblePanel');
+  electricalPanel.classList.add('hiddenPanel');
+  softwarePanel.classList.remove('visiblePanel');
+  softwarePanel.classList.add('hiddenPanel');
+  mechanicalPanel.classList.remove('visiblePanel');
+  mechanicalPanel.classList.add('hiddenPanel');
 });
